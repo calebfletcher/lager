7514d419fa7250f93bb1329157c1960ce94f338f
Caleb Fletcher <caleb@fletcher.cf>
2021-11-03 21:56:48 +1100

    feat: add fixed 3x3 matrix implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mtx = Matrix::new();
+    dbg!("{}", mtx);
+}
+
+#[derive(Debug)]
+struct Matrix {
+    values: [[f32; 3]; 3],
+}
+
+impl Matrix {
+    fn new() -> Self {
+        Self {
+            values: [[0.0; 3]; 3],
+        }
+    }
 }

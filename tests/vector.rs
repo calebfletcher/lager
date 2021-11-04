d5d6cec0e17d78d50541c99c97ffa6e2bcc64867
Caleb Fletcher <caleb@fletcher.cf>
2021-11-04 20:59:52 +1100

    test: add test for matrix-vector multiplication

--- tests/vector.rs
+++ tests/vector.rs
@@ -1,21 +1,32 @@
 use lager::{Matrix, Vector};
 
 #[test]
-fn add_vector() {
-    let vec1: Vector<f32, 3> = Vector::new([[1.0], [2.0], [3.0]]);
-    let vec2: Vector<f32, 3> = Vector::new([[11.0], [21.0], [31.0]]);
+fn add_3x1_vectors_elementwise() {
+    let vec1: Vector<f64, 3> = Vector::new([[1.0], [2.0], [3.0]]);
+    let vec2: Vector<f64, 3> = Vector::new([[11.0], [21.0], [31.0]]);
     let res = vec1 + vec2;
 
-    let expected: Vector<f32, 3> = Vector::new([[12.0], [23.0], [34.0]]);
+    let expected: Vector<f64, 3> = Vector::new([[12.0], [23.0], [34.0]]);
     assert!(res.isclose(&expected));
 }
 
 #[test]
-fn mult_vector_elementwise() {
-    let vec1: Vector<f32, 3> = Vector::new([[1.0], [2.0], [3.0]]);
-    let vec2: Vector<f32, 3> = Vector::new([[11.0], [21.0], [31.0]]);
-    let res: Vector<f32, 3> = vec1 * vec2;
+fn multiply_3x1_vectors_elementwise() {
+    let vec1: Vector<f64, 3> = Vector::new([[1.0], [2.0], [3.0]]);
+    let vec2: Vector<f64, 3> = Vector::new([[11.0], [21.0], [31.0]]);
+    let res: Vector<f64, 3> = vec1 * vec2;
 
-    let expected: Vector<f32, 3> = Vector::new([[11.0], [42.0], [93.0]]);
+    let expected: Vector<f64, 3> = Vector::new([[11.0], [42.0], [93.0]]);
+    assert!(res.isclose(&expected));
+}
+
+#[test]
+fn multiply_3x3_matrix_with_3x1_vector() {
+    let mtx = Matrix::new([[1.0, 0.0, -2.0], [0.0, 3.0, -1.0], [1.0, 2.0, 1.0]]);
+    let vec: Vector<f64, 3> = Vector::new([[3.0], [-1.0], [4.0]]);
+
+    let res: Vector<f64, 3> = mtx.mul(&vec);
+
+    let expected: Vector<f64, 3> = Vector::new([[-5.0], [-7.0], [5.0]]);
     assert!(res.isclose(&expected));
 }

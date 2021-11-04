7b2811439bf978a31f5441a7b799575471416c43
Caleb Fletcher <caleb@fletcher.cf>
2021-11-04 21:03:11 +1100

    test: use isclose for matrix comparison

--- tests/matrix.rs
+++ tests/matrix.rs
@@ -11,28 +11,31 @@ fn index_matrices() {
 }
 
 #[test]
-fn matrices_are_close() {
+fn check_matrices_are_close() {
     let mtx1 = Matrix::new([[4.4, 7.3, 2.5], [2.4, 6.4, 2.2], [9.5, 3.5, 4.6]]);
     let mtx2 = Matrix::new([
         [4.40004, 7.299998, 2.50002],
         [2.39998, 6.400001, 2.19998],
         [9.50006, 3.499997, 4.60004],
     ]);
+
     assert!(mtx1.isclose(&mtx2));
 }
 
 #[test]
-fn multiply_identity_matrices() {
+fn multiply_3x3_identity_matrices() {
     let i3 = Matrix::new([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]);
+
     let res = i3.mul(&i3);
 
-    assert_eq!(res, i3);
+    assert!(res.isclose(&i3));
 }
 
 #[test]
 fn multiply_2x2_matrices_elementwise() {
     let mtx1 = Matrix::new([[1.0, 2.0], [3.0, 4.0]]);
     let mtx2 = Matrix::new([[4.0, 6.0], [7.0, 5.0]]);
+
     let res = mtx1 * mtx2;
 
     assert!(res.isclose(&Matrix::new([[4.0, 12.0], [21.0, 20.0]])));
@@ -42,19 +45,20 @@ fn multiply_2x2_matrices_elementwise() {
 fn multiply_3x3_matrices() {
     let mtx1 = Matrix::new([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
     let mtx2 = Matrix::new([[1.0, 2.0, 1.0], [2.0, 4.0, 6.0], [7.0, 2.0, 5.0]]);
-    let res = mtx1.mul(&mtx2);
 
-    assert_eq!(
-        res,
-        Matrix::new([[26.0, 16.0, 28.0], [56.0, 40.0, 64.0], [86.0, 64.0, 100.0]])
-    );
+    let res = mtx1.mul(&mtx2);
+    let expected = Matrix::new([[26.0, 16.0, 28.0], [56.0, 40.0, 64.0], [86.0, 64.0, 100.0]]);
+
+    assert!(res.isclose(&expected));
 }
 
 #[test]
 fn multiply_matrices_different_shapes() {
     let mtx1 = Matrix::new([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]);
     let mtx2 = Matrix::new([[10.0, 11.0], [20.0, 21.0], [30.0, 31.0]]);
-    let res = mtx1.mul(&mtx2);
 
-    assert_eq!(res, Matrix::new([[140.0, 146.0], [320.0, 335.0]]));
+    let res = mtx1.mul(&mtx2);
+    let expected = Matrix::new([[140.0, 146.0], [320.0, 335.0]]);
+
+    assert!(res.isclose(&expected));
 }
--- tests/vector.rs
+++ tests/vector.rs
@@ -4,9 +4,10 @@ use lager::{Matrix, Vector};
 fn add_3x1_vectors_elementwise() {
     let vec1: Vector<f64, 3> = Vector::new([[1.0], [2.0], [3.0]]);
     let vec2: Vector<f64, 3> = Vector::new([[11.0], [21.0], [31.0]]);
-    let res = vec1 + vec2;
 
+    let res = vec1 + vec2;
     let expected: Vector<f64, 3> = Vector::new([[12.0], [23.0], [34.0]]);
+
     assert!(res.isclose(&expected));
 }
 
@@ -14,9 +15,10 @@ fn add_3x1_vectors_elementwise() {
 fn multiply_3x1_vectors_elementwise() {
     let vec1: Vector<f64, 3> = Vector::new([[1.0], [2.0], [3.0]]);
     let vec2: Vector<f64, 3> = Vector::new([[11.0], [21.0], [31.0]]);
-    let res: Vector<f64, 3> = vec1 * vec2;
 
+    let res: Vector<f64, 3> = vec1 * vec2;
     let expected: Vector<f64, 3> = Vector::new([[11.0], [42.0], [93.0]]);
+
     assert!(res.isclose(&expected));
 }
 
@@ -26,7 +28,7 @@ fn multiply_3x3_matrix_with_3x1_vector() {
     let vec: Vector<f64, 3> = Vector::new([[3.0], [-1.0], [4.0]]);
 
     let res: Vector<f64, 3> = mtx.mul(&vec);
-
     let expected: Vector<f64, 3> = Vector::new([[-5.0], [-7.0], [5.0]]);
+
     assert!(res.isclose(&expected));
 }
